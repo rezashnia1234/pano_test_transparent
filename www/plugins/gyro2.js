619530cdb6c15f2207f46a436f14ad07b03d3d22
rezashnia1234 <reza_shnia@spam4.me>
2017-03-23 05:14:33 +0430

    0

--- www/plugins/gyro2.js
+++ www/plugins/gyro2.js
@@ -178,8 +178,9 @@ var krpanoplugin = function() {
 			}
 			window.sessionStorage.removeItem("temp_delta");
 			window.sessionStorage.removeItem("temp_compass");
+			window.sessionStorage.setItem('compass_used',"yes");
 		}
-		if (window.cordova)
+		if (window.cordova && window.sessionStorage.getItem('compass_used') == null)
 		{
 			if (typeof compassSuccess !== 'undefined' && $.isFunction(compassSuccess)) {
 				
